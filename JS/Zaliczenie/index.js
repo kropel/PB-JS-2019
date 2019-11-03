3911a2cfed7ab85836cc6022c5d2691019a07fd4
Radosław Kuczyński <krooopel@gmail.com>
2019-11-03 15:05:54 +0100

    Zadanie na zaliczenie

--- JS/Zaliczenie/index.js
+++ JS/Zaliczenie/index.js
@@ -1,11 +1,122 @@
-function getRandomNumber(max, min = 0){
-    return Math.floor(Math.random()*max + min);
-}
-let tab = Array(getRandomNumber(12,8)).fill(1);
-    tab[getRandomNumber(tab.length)]= 2;
-    console.table(tab);
-    if (tab.length % 2 === 0){      //nieparzysta liczba elementow
-        while()
-    } else {                        //parzysta liczba elementow
+let balls = Array(getRandomNumber(200, 8)).fill(1),
+    indexesBalls = balls.map((v, i) => i),
+    numberOFIteration = 0;
 
+balls[getRandomNumber(balls.length)] = 2;
+
+indexesBalls = getRandomArray(indexesBalls);
+
+function getRandomArray(orginalArray) {
+    let tempArray = [...orginalArray],
+        randomArray = [];
+    while (tempArray.length > 0) {
+        randomArray.push(tempArray.splice(getRandomNumber(tempArray.length), 1)[0]);
     }
+    return randomArray;
+}
+
+function getRandomNumber(max, min = 0) {
+    return Math.floor(Math.random() * max + min);
+}
+
+function sum(indexes) {
+    return indexes.reduce((previous, current) => previous + balls[current], 0);
+}
+
+function findDifferendPartArray(tempArray, breakPoint) {
+    let indexX = tempArray.splice(0, breakPoint),
+        indexY = tempArray.splice(0, breakPoint),
+        sumX = sum(indexX),
+        sumY = sum(indexY);
+
+    if (sumX === sumY) {
+        if (tempArray.length === 1) {
+            return tempArray[0];
+        } else {
+            return getBall(tempArray);
+        }
+    } else if (sumX - sumY > 0) {
+        return getBall(indexX);
+    } else {
+        return getBall(indexY);
+    }
+}
+
+function getBall(indexes) { // 'indexes' is an array of indexes balls array
+    let length = indexes.length,
+        breakPoint = 0;
+
+    numberOFIteration++;
+    console.log('Iteration: ' + numberOFIteration + ` whit array(${length}): ` + indexes + "\n");
+
+    switch(true){
+    case (length === 1):
+        return indexes[0];
+
+    case (length === 2):
+        return (balls[indexes[0]] > balls[indexes[1]] ? indexes[0] : indexes[1]);
+
+    case (length === 3):
+        if (balls[indexes[0]] - balls[indexes[1]] === 0) { // if balls on indexes 0 and 1 are equal return index of third ball
+            return indexes[2];
+        } else {                                        // if balls on indexes 0 and 1 are NOT equal call getBall function without last index
+            indexes.pop();
+            return getBall(indexes);
+        }
+
+    case (length === 4):
+        indexX = indexes.splice(0, 2);
+        if (sum(indexX) > sum(indexes)) {
+            return getBall(indexX);
+        } else {
+            return getBall(indexes);
+        }
+        
+    case (length === 5):
+        breakPoint = 2;
+        return findDifferendPartArray(indexes, breakPoint);
+
+    case (length % 2 === 0):           // even
+        breakPoint = (length - 2) / 2;  // break point array 'indexes' without last two elements 
+        return findDifferendPartArray(indexes, breakPoint);
+        
+    case (length % 2 != 0):             // odd 
+        breakPoint = (length - 3) / 2;  // break point array 'indexes' without last tree elements 
+        return findDifferendPartArray(indexes, breakPoint);
+    }//switch
+
+
+    // if (length === 1) {
+    //     return indexes[0];
+    // }else if (length === 2) {
+    //     return (balls[indexes[0]] > balls[indexes[1]] ? indexes[0] : indexes[1]);
+    // }else if (length === 3) {
+    //     if (balls[indexes[0]] - balls[indexes[1]] === 0) { // if balls on indexes 0 and 1 are equal return index of third ball
+    //         return indexes[2];
+    //     } else {                                        // if balls on indexes 0 and 1 are NOT equal call getBall function without last index
+    //         indexes.pop();
+    //         return getBall(indexes);
+    //     }
+    // }else if (length === 4) {
+    //     indexX = indexes.splice(0, 2);
+    //     if (sum(indexX) > sum(indexes)) {
+    //         return getBall(indexX);
+    //     } else {
+    //         return getBall(indexes);
+    //     }
+    // }else if (length === 5) {
+    //     breakPoint = 2;
+    //     return findDifferendPartArray(indexes, breakPoint);
+    // }else if (length % 2 === 0) {           // even
+    //     breakPoint = (length - 2) / 2;  // break point array 'indexes' without last two elements 
+    //     return findDifferendPartArray(indexes, breakPoint);
+    // }else if (length % 2 != 0) {             // odd 
+    //     breakPoint = (length - 3) / 2;  // break point array 'indexes' without last tree elements 
+    //     return findDifferendPartArray(indexes, breakPoint);
+    // }
+}
+
+
+let indexOfBall = getBall(indexesBalls);
+console.log('Index of ball with "' + balls[indexOfBall] + '" => ' + indexOfBall + ' at array: ' + balls);
+console.log('Number of calls getBall() function: ' + numberOFIteration);
--- JS/Zjazd3/homeWark.js
+++ JS/Zjazd3/homeWark.js
@@ -124,3 +124,92 @@
 // *****************************************************
 // 3) Create an array (by hand) of objects and call sum() function in context of each one of them. Sum function is to come from this object  BaseObject = { X,y, sum: function (){ return this.X+this.y}}
 
+// let baseObject = { 
+//     x: 4,
+//     y: 5, 
+//     sum: function (){ return this.x+this.y; }
+//     };
+
+// let arrayOfObject = [
+//     {x: 11, y: 23},
+//     {x: 345, y: 12},
+//     {x: 11, y: 23},
+//     {x: 52, y: 24},
+//     {x: 89, y: 4},
+//     {x: 74, y: 34},
+//     {x: 11, y: 46},
+//     {x: 76, y: 3}
+// ];
+
+// console.log( baseObject.sum() );
+// arrayOfObject.forEach( elem => console.log( baseObject.sum.call(elem) ));
+
+// for(let i = 0; i < arrayOfObject.length; i++){
+//     let sum = baseObject.sum.call( arrayOfObject[i] );
+//     console.log( sum );
+// }
+
+// *****************************************************
+// 4) Given an array of objects, for each object call operation() function in context of next object. If object is last, got back to start of the array for operation function. In loop;
+
+// let arrayOfObject =
+//     [
+//         {
+//             x: 1,
+//             y: 'object one value',
+//             operation: (obj) => 'object one prafix ' + obj.x + " " + obj.y
+//         },
+//         {
+//             x: 2,
+//             y: 'object two value',
+//             operation: (obj) => 'object two prefix ' + obj.x + " " + obj.y
+//         },
+//         {
+//             x: 3,
+//             y: 'object three value',
+//             operation: (obj) => 'object three prefix ' + obj.x + " " + obj.y
+//         },
+//     ];
+
+// arrayOfObject.forEach((elem, index) => {
+//     if (index >= arrayOfObject.length - 1) {
+//         console.log(elem.operation(arrayOfObject[0]));
+//     } else {
+//         console.log(elem.operation(arrayOfObject[index + 1]));
+//     }
+// });
+
+let arrayOfObject2 =
+    [
+        {
+            x: 1,
+            y: 'object one value',
+            operation: function () { return ('object one prafix ' + this.x + " " + this.y); }
+        },
+        {
+            x: 2,
+            y: 'object two value',
+            operation: function () { return ('object two prefix ' + this.x + " " + this.y); }
+        },
+        {
+            x: 3,
+            y: 'object three value',
+            operation: function () { return ('object three prefix ' + this.x + " " + this.y); }
+        },
+    ];
+//********* BIND
+arrayOfObject2.forEach((elem, index) => {
+    if (index >= arrayOfObject2.length - 1) {
+        console.log(elem.operation.bind(arrayOfObject2[0])());
+    } else {
+        console.log(elem.operation.bind(arrayOfObject2[index + 1])());
+    }
+});
+//********* CALL
+arrayOfObject2.forEach((elem, index) => {
+    if (index >= arrayOfObject2.length - 1) {
+        console.log(elem.operation.call(arrayOfObject2[0]));
+    } else {
+        console.log(elem.operation.call(arrayOfObject2[index + 1]));
+    }
+});
